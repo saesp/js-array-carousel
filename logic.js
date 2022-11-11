2c686f053dc3fd2872549381a5142d34768e54c9
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-11 16:10:31 +0100

    ...

---
+++ logic.js
@@ -0,0 +1,11 @@
+// Creare un blocco di immagini dinamico
+
+
+// creare array contenente le img
+const img1 = ;
+const img2 = ;
+const img3 = ;
+const img4 = ;
+const img5 = ;
+
+const imgList = [img1, img2, img3, img4, img5];
