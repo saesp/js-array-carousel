e3830f8b22bcd96960e3d771be88226dc465064d
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-14 11:41:33 +0100

    ,

--- logic.js
+++ logic.js
@@ -1,53 +1,39 @@
 // Creare un blocco di immagini da 1 a 5
 
 
-let imgList = new Array();
+let imgList = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
 
-imgList[0] = new Image();
-imgList[0].src = 'img/01.webp';
+const contCar = document.querySelector("div.cont-car");
+let carContent = " ";
 
-imgList[1] = new Image();
-imgList[1].src = 'img/02.webp';
-
-imgList[2] = new Image();
-imgList[2].src = 'img/03.webp';
-
-imgList[3] = new Image();
-imgList[3].src = 'img/04.webp';
-
-imgList[4] = new Image();
-imgList[4].src = 'img/05.webp';
-
+let active = 0;
 
 for (let i = 0; i < imgList.length; i++) {
-    const contCar = document.querySelector("div.container-carousel");
-    const div = document.createElement("div");
-
-    div.append(imgList[i]);
-    contCar.append(div);
     
-    div.classList.add("cont-img");
-
-    console.log(imgList[i]);
+    carContent += `
+    <div class="cont-img">
+        <img src="${imgList[i]}">
+    </div>`
 }
+
+contCar.innerHTML += carContent;
+document.getElementsByClassName('cont-img')[active].classList.add('active');
 
 
 const butUp = document.querySelector(".but-up");
 
-const contImg = document.getElementsByClassName("cont-img");
-contImg[0].classList.add("active");
-let activeItem = 0;
-
-// creo azione click button
 butUp.addEventListener("click", function (){
     
-    // se img < numTot img allora vai avanti
-    if (contImg < contImg.length - 1){
-        contImg[activeItem].classList.remove("active");
-        activeItem++;
-        contImg[activeItem].classList.add("active");
-    } 
+    if (active == contImg.length - 1){
+        active = 0;
+    } else {
+        active++;
+    }
+
+    document.querySelector('.cont-img.active').classList.remove("active");
+    document.getElementsByClassName('cont-img')[active].classList.add("active");
 })
+
 
 
 
